beb042ad63bdd7539fcfb035ed055588b142e274
Neroimor <neroimor@gmail.com>
2026-04-30 01:52:27 +0300

    Added entitie Client

--- Teledoc.ApiServices/DataBase/Context/AppDbContext.cs
+++ Teledoc.ApiServices/DataBase/Context/AppDbContext.cs
@@ -14,9 +14,71 @@ namespace Teledoc.ApiServices.DataBase.Context
         {
             base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Client>(entity =>
+            {
+                entity.HasKey(x => x.Id);
 
+                entity.Property(x => x.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(x => x.TaxpayerIndividualNumber)
+                    .IsRequired()
+                    .HasColumnType("varchar(12)");
+
+                entity.Property(x => x.TypeRole)
+                    .IsRequired()
+                    .HasColumnType("varchar(20)");
+
+                entity.Property(x => x.CreatedAt)
+                    .IsRequired();
+
+                entity.Property(x => x.UpdatedAt)
+                    .IsRequired();
+
+                entity.HasMany(x => x.Founders)
+                    .WithOne(x => x.Client)
+                    .HasForeignKey(x => x.ClientId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasIndex(x => x.TaxpayerIndividualNumber)
+                    .IsUnique();
+            });
+
+            modelBuilder.Entity<Founder>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x => x.ClientId)
+                    .IsRequired();
+
+                entity.Property(x => x.TaxpayerIndividualNumber)
+                    .IsRequired()
+                    .HasColumnType("varchar(12)");
+
+                entity.Property(x => x.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(x => x.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(x => x.MiddleName)
+                    .HasMaxLength(50);
+
+                entity.Property(x => x.CreatedAt)
+                    .IsRequired();
+
+                entity.Property(x => x.UpdatedAt)
+                    .IsRequired();
+
+                entity.HasIndex(x => new { x.ClientId, x.TaxpayerIndividualNumber })
+                    .IsUnique();
+            });
         }
 
         public DbSet<Founder> Founders { get; set; }
+        public DbSet<Client> Clients { get; set; }
     }
 }
--- Teledoc.ApiServices/DataBase/Entities/Client.cs
+++ Teledoc.ApiServices/DataBase/Entities/Client.cs
@@ -1,24 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Teledoc.ApiServices.Validators;
 
 namespace Teledoc.ApiServices.DataBase.Entities
 {
-
-    public class Founder
+    public class Client
     {
-
         public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
         public string TaxpayerIndividualNumber { get; set; } = string.Empty;
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string? MiddleName { get; set; }
-
+        public string TypeRole { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
-        public int ClientId { get; set; }
-        public Client Client { get; set; } = null!;
-
+        public ICollection<Founder> Founders { get; set; } = [];
     }
 }
